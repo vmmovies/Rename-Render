334563c80a58195119f84ab4891b3d5c272c7d38
vmmovies <154879321+vmmovies@users.noreply.github.com>
2024-01-07 14:09:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ API_HASH = os.environ.get("API_HASH", "dc4e9659b934f1e63913772aa2e5f27c")
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "6726180488:AAH9tIf9Hp7r0zhvjXfUCM7ZSPXUbDfK84s") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "VM_Peace") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "https://t.me/+VE_6wbNkYbFjZWU1") 
 
              # Don't Remove Credit @VJ_Botz
              # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
@@ -27,7 +27,7 @@ FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '1640326738').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6258598643').split()]
 
 PORT = os.environ.get("PORT", "8080")
 
